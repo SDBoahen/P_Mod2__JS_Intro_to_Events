df223f9db07dcf49b0ecb8c9231773bd7c72a326
SDBoahen <SDBoahen@gmail.com>
2023-06-22 11:07:09 -0400

    The starting of the Big3: HTML + CSS + JS, plus 1 querySelector for an H1

---
+++ main.js
@@ -0,0 +1,16 @@
+console.log( "We Are Live? 👀" )
+
+// Document OBJECT Model ~ The DOM
+
+
+const h1OnTheDOM = document.querySelector("h1")
+    console.log( "Did We Get It? 👀: " , h1OnTheDOM )  //
+
+    h1OnTheDOM.textContent = "10.1 is Lit! 🙌"
+
+
+    // const h1 = document.querySelector( "h1" )
+    // console.log( "Did We Get It? 👀: " , h1 )  //
+
+    // h1.textContent = "10.1 is Lit! 🙌"
+
